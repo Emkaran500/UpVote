6415726e433292d6302ad2067c1d83e4363bc1aa
Emil <emkaran_500@mail.ru>
2024-03-30 23:15:27 +0100

    Add Server Creation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Text.Json;
+
+var httpListener = new HttpListener();
+
+var prexif = "http://*:55555/";
+
+httpListener.Prefixes.Add(prexif);
+
+httpListener.Start();
+
+Console.WriteLine($"Server started...");
+
+while (true)
+{
+    var client = await httpListener.GetContextAsync();
+
+    string? endpoint = client.Request.RawUrl;
+
+    client.Response.Close();
+}
